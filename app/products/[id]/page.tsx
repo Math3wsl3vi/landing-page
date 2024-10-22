c85e68a277c83b7f97f26b2b620d0e0ce8762c66
math3wsl3vi <levimathews9841@gmail.com>
2024-10-22 21:15:22 +0300

    sir

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -51,35 +51,47 @@ export default async function ProductPage({ params }: ProductPageProps) {
         />
        </div>
         <div className='flex flex-row gap-3 cursor-pointer'>
-        <Image src={product.image} alt='rate' width={71} height={70}/>
-        <Image src={product.image} alt='rate' width={71} height={70}/>
-        <Image src={product.image} alt='rate' width={71} height={70}/>
-        <Image src={product.image} alt='rate' width={71} height={70}/>
-        {/* <Image src={product.image} alt='rate' width={70} height={70}/> */}
+        <Image src={product.image} alt='rate' width={69} height={70}/>
+        <Image src={product.image} alt='rate' width={69} height={70}/>
+        <Image src={product.image} alt='rate' width={69} height={70}/>
+        <Image src={product.image} alt='rate' width={69} height={70}/>
+        <Image src={product.image} alt='rate' width={69} height={70}/>
         </div>
         </div>
        <div className='md:items-center px-3'>
-       <h1 className='text-2xl font-medium'>{product.name}</h1>
-       <h1 className='text-primary font-medium'>ksh {product.price}</h1>
-       <div className='flex gap-2 my-3'>
+       <h1 className='text-2xl font-medium font-sans text-meow'>{product.name}</h1>
+       <h1 className='text-primary font-medium text-2xl'>ksh <span className='font-semibold'>{product.price}</span></h1>
+       {/* <div className='flex gap-2 my-3'>
         <Image src='/star.png' alt='rate' width={20} height={20}/>
         <Image src='/star.png' alt='rate' width={20} height={20}/>
         <Image src='/star.png' alt='rate' width={20} height={20}/>
         <Image src='/star.png' alt='rate' width={20} height={20}/>
         <Image src='/star.png' alt='rate' width={20} height={20}/>
 
-       </div>
+       </div> */}
        <h1 className='text-meow text-sm font-sans'>{product.desc}</h1>
        <h2 className='text-sm mt-5'>Scientific name: <span className='font-semibold'>krukuta krukuta</span></h2>
        <h2 className='text-sm mt-2'>Age: <span className='font-semibold'>4 weeks</span></h2>
+       <h2 className='text-sm mt-2'>Specifications: <span className='font-semibold'>Water daily</span></h2>
        <div className='my-10 flex flex-col gap-5'>
-        <div className='flex flex-col gap-1'>
-        <label htmlFor="name">Quantity</label>
-        <input type="text" className='border border-gray-600'/>
+        <div className='flex flex-row gap-1 items-center'>
+        <label htmlFor="options">Quantity:</label>
+        <select name="option" id="options" className='bg-white border border-gray-300 p-2 outline-none cursor-pointer'>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
+        </select>
         </div>
         <Button type='button' title='Add to cart'/>
         </div>
-        <div className='flex flex-col gap-3'>
+        {/* <div className='flex flex-col gap-3'>
             <h1>Share this Product:</h1>
             <div className='flex gap-5'>
             <div className='cursor-pointer'>
@@ -92,11 +104,7 @@ export default async function ProductPage({ params }: ProductPageProps) {
             <Image src='/whatsapp.png' alt='rate' width={20} height={20}/>
             </div>
             </div>
-            
-            {/* <div>
-            <Image src='/facebook.png' alt='rate' width={20} height={20}/>
-            </div> */}
-        </div>
+        </div> */}
        </div>    
         </div>
         <Description product={product}/>
